741f066853e5281d877b34cab63eb2060ea2d569
Abhishek Simla <abhishek.130490@gmail.com>
2018-01-14 13:33:01 +0530

    Added reducer is store

--- ui/src/store.js
+++ ui/src/store.js
@@ -2,9 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import gameReducer from './game/gameReducer';
 
 export default createStore(
-  combineReducers({}),
+  combineReducers({
+    game: gameReducer
+  }),
   {},
   applyMiddleware(createLogger(), thunk, promise())
 );

aa7e41b5d4fa641ea7405f29d967033f624b9311
Abhishek Simla <abhishek.130490@gmail.com>
2018-01-20 13:15:21 +0530

    Added main app module in typescript Added types for socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,27 @@
-const a: number = 1;
-console.log(a);
+// const logger = require('./log4js').logger;
+
+import * as express from 'express';
+import * as path from 'path';
+import * as sio from 'socket.io';
+import { Request, Response } from 'express';
+
+const app = express();
+const server = require('http').Server(app);
+const io = sio(server);
+
+io.on('connection', require('./socketConnectionHandler'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello');
+  res.end();
+});
+
+const port = process.env.PORT || 12345;
+server.listen(port);
+
+// logger.info(`Multiplayer server listening on ${port}`);

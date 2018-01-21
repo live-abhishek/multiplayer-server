6266f7b22609698c93ff4914f3009bb6f3e85626
Abhishek Simla <abhishek.130490@gmail.com>
2018-01-21 19:56:54 +0530

    Fixed a bug while referencing the UI project

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,10 @@ const server = require('http').Server(app);
 const io = sio(server);
 
 io.on('connection', socketConnectionHandler);
-app.use(express.static(path.join(__dirname, 'ui/build')));
+app.use(express.static(path.join(__dirname, '..', 'u', 'build')));
 
 app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, 'ui/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'ui', 'build', 'index.html'));
 });
 
 app.get('/hello', (req: Request, res: Response) => {

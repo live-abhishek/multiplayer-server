194eb58a6c583379e60c05940a70f3aa177ad971
Abhishek Simla <abhishek.130490@gmail.com>
2018-01-23 00:00:28 +0530

    Updated npm watch & watch-debug scripts Converted a comment to JsDoc comment

--- src/iRoom.ts
+++ src/iRoom.ts
@@ -2,7 +2,9 @@ export interface IRoom {
   roomName: string;
   gameType: string;
   players: Array<any>;
-  // socket is regarded as player, so the two are equal in the context of room
+  /**
+   * socket is regarded as player, so the two are equal in the context of room
+   */
   addPlayer(socket: any): void;
   isGameOver(): boolean;
   isAvailable(): boolean;

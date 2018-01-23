6ee8f6ead3ff456ffbb145d6690f2fafd9d12859
Masterchief <live.abhishek@outlook.com>
2018-01-23 12:47:06 +0530

    Updated some symbol names

--- ui/src/game/gameArea.js
+++ ui/src/game/gameArea.js
@@ -3,24 +3,24 @@ import TicTacToe from './tic-tac-toe/tic-tac-toe';
 import GameMenu from './gameMenu';
 import CircularIndeterminate from './gameWaiting';
 import { connect } from 'react-redux';
-import { requestGame } from './gameAction';
+import gameAction from './gameAction';
 import socket from '../socketProvider';
 
 const gameArea = (props) => {
   return (
     // <TicTacToe />
     <div>
-      {props.gameState === 'MENU' && <GameMenu requestHandler={props.requestGame} />}
-      {props.gameState === 'WAITING' && <CircularIndeterminate />}
-      {props.gameState === 'FULFILLED' && props.gameType === 'tictactoe' && <TicTacToe />}
-      {props.gameState === 'FULFILLED' && props.gameType === 'tictactoe2' && <div>TicTacToe2</div>}
+      {props.requestState === 'MENU' && <GameMenu requestHandler={props.requestGame} />}
+      {props.requestState === 'WAITING' && <CircularIndeterminate />}
+      {props.requestState === 'FULFILLED' && props.gameType === 'tictactoe' && <TicTacToe />}
+      {props.requestState === 'FULFILLED' && props.gameType === 'tictactoe2' && <div>TicTacToe2</div>}
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    gameState: state.game.gameState,
+    gameState: state.game.requestState,
     gameType: state.game.gameType
   }
 }
@@ -29,7 +29,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     requestGame: (gameType) => {
       socket.emit('gameRequest', { gameType })
-      dispatch(requestGame(gameType));
+      dispatch(gameAction.requestGame(gameType));
     }
   }
 }

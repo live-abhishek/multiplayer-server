121e7bbf605f3c6909f11821c1cdd8f289036986
Masterchief <live.abhishek@outlook.com>
2018-01-31 19:36:13 +0530

    Removed unnecessary styles

--- ui/src/game/gameMenu.js
+++ ui/src/game/gameMenu.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { withStyles } from "material-ui/styles";
 import Button from "material-ui/Button";
 import SimpleMediaCard from "../components/simpleMediaCard";
 import tictactoeImage from "./tic-tac-toe/tic-tac-toe.png";
-
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit
-  },
-  input: {
-    display: "none"
-  }
-});
 
 const gameMenu = props => {
   const clickHandler = param => () => {
@@ -32,14 +22,8 @@ const gameMenu = props => {
         title="Tic-Tac-Toe-2"
         detail="double blah"
       />
-      <Button raised color="primary" className={classes.button}>
-        Tic-Tac-Toe-1
-      </Button>
-      <Button raised color="accent" className={classes.button}>
-        Tic-Tac-Toe-2
-      </Button>
     </div>
   );
 };
 
-export default withStyles(styles)(gameMenu);
+export default gameMenu;

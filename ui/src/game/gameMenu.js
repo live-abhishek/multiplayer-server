bbcbeafa858abd29bd5370f417136b7740cf0686
Masterchief <live.abhishek@outlook.com>
2018-02-01 18:59:34 +0530

    Added a coming soon pic

--- ui/src/game/gameMenu.js
+++ ui/src/game/gameMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SimpleMediaCard from "../components/simpleMediaCard";
 import tictactoeImage from "./tic-tac-toe/ttt.jpg";
+import comingSoonImage from "./coming-soon.png";
 import "./gameMenu.css";
 
 const gameMenu = props => {
@@ -14,15 +15,15 @@ const gameMenu = props => {
           onClick={clickHandler("tictactoe")}
           image={tictactoeImage}
           title="Tic-Tac-Toe-1"
-          detail="blah blah blah"
+          detail="Play a quick game with a random person around the globe"
         />
-      </div >
+      </div>
       <div className="item">
         <SimpleMediaCard
-          onClick={clickHandler("tictactoe2")}
-          image={tictactoeImage}
-          title="Tic-Tac-Toe-2"
-          detail="double blah"
+          // onClick={clickHandler("tictactoe2")}
+          image={comingSoonImage}
+          title="Coming Soon"
+          detail="Stay Tuned"
         />
       </div>
     </div>

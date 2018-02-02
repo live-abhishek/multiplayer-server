47b9802a4fae2ef815fc1c9c927576f7e4619f50
Masterchief <live.abhishek@outlook.com>
2018-02-02 13:09:48 +0530

    Updated the CSS of the waiting page Added break lines in the waiting message

--- ui/src/components/customLoading.js
+++ ui/src/components/customLoading.js
@@ -1,18 +1,20 @@
 import React from "react";
-import "./customLoading.css"
+import "./customLoading.css";
 import gameImage from "./game.png";
 
-export const CustomLoading = (props) => {
+export const CustomLoading = props => {
   return (
     <div className="grid">
       <div className="loading">
         <img src={gameImage} />
-        <div className="button orange"></div>
-        <div className="button red"></div>
-        <div className="button green"></div>
-        <div className="button blue"></div>
+        <div className="button orange" />
+        <div className="button red" />
+        <div className="button green" />
+        <div className="button blue" />
       </div>
-      <div className="wait-msg">Finding an opponent. You can ask you friend to join.</div>
+      <div className="wait-msg">
+        Finding an opponent!<br /> You can ask you friend to join.
+      </div>
     </div>
-  )
-}
+  );
+};

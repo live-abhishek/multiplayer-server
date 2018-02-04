c8a1c2bcad70451167b42cf1069e357c45c6b3db
Abhishek Simla <abhishek.130490@gmail.com>
2018-02-04 11:29:41 +0530

    Added code to render the cells properly Added styles to the board

--- ui/src/game/dots/dots.js
+++ ui/src/game/dots/dots.js
@@ -2,19 +2,14 @@ import React from "react";
 import LinearIndeterminate from "../../animations/linearIndeterminate";
 import ScoreBoard from "../../components/scoreBoard/scoreBoard";
 import Popout from "../../animations/popout";
+import "./dots.css";
 
 class Dots extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      boardState =[
-        [1, 1],
-        [1, 1, 1, 0, 1]
-        [1, 2],
-        [1, 1, 2, 2, 2]
-        []
-      ]
-    }
+      board: [[1, 1], [1, 1, 1, 0, 1], [1, 2], [1, 1, 2, 2, 2], [2, 2]]
+    };
   }
 
   getWaitMessage = () => {
@@ -24,51 +19,47 @@ class Dots extends React.Component {
     }
   };
 
-  createDots = (count) => {
+  createDots = count => {
     const dots = [];
     for (let i = 0; i < count; i++) {
-      dots.push(
-        <div className="dot"></div>
-      )
+      dots.push(<div className="dot" />);
     }
     return dots;
-  }
+  };
 
-  renderHorizontalLineRow = (row) => {
+  renderHorizontalLineRow = row => {
     const cellHorLines = row.map(cell => {
       return (
-        <div className="hor-line">
+        <div className="hor-cell">
+          <div className="hor-line" />
         </div>
-      )
+      );
     });
     const dots = this.createDots(row.length + 1);
     // mix dots & horLines alternately, with dots first
-    const horLine = []
+    const horLine = [];
     for (let i = 0; i < row.length; i++) {
       horLine.push(dots[i]);
       horLine.push(cellHorLines[i]);
     }
     horLine.push(dots[row.length]);
     return horLine;
-  }
+  };
 
-  renderVerticalLineRow = (row) => {
-    const cellVerLines = (row.map(cell => {
-      return (
-        <div className="ver-line">
-        </div>
-      )
-    }));
-    const dots = this.createDots(row.length - 1);
-    // mix dots & verLines alternately, with line first
-    const verLine = [];
-    for (let i = 0; i < row.length; i++) {
-      verLine.push(cellVerLines[i]);
-      verLine.push(dots[i]);
-    }
-    verLine.push(cellVerLines[row.length]);
+  renderVerticalLineRow = row => {
+    const verLine = row.map((cell, colIndex) => {
+      if (colIndex % 2) {
+        return <div className="box" />;
+      } else {
+        return (
+          <div className="ver-cell">
+            <div className="ver-line" />
+          </div>
+        );
+      }
+    });
     return verLine;
-  }
+  };
 
   renderRow = (row, rowIndex) => {
     if (rowIndex % 2) {
@@ -76,10 +67,10 @@ class Dots extends React.Component {
     } else {
       return this.renderHorizontalLineRow(row);
     }
-  }
+  };
 
   render() {
-    const { board } = this.state.board;
+    const { board } = this.state;
     return (
       <div className="center-area">
         <div className="score-holder">
@@ -87,10 +78,12 @@ class Dots extends React.Component {
         </div>
         <div className="board">
           {board.map((row, rowIndex) => {
-            this.renderRow(row, rowIndex);
+            return this.renderRow(row, rowIndex);
           })}
         </div>
       </div>
     );
   }
 }
+
+export default Dots;

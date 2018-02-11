d322e7d999af841ad1efe73caffa3bc634b202a8
Abhishek Simla <abhishek.130490@gmail.com>
2018-02-11 08:59:46 +0530

    Refactored dots process move

--- src/dots/dotsMatch.ts
+++ src/dots/dotsMatch.ts
@@ -1,8 +1,10 @@
+import { logger } from "../bunyan";
 import {
   DotsResponseEvent,
   ResponseDotsMatchResultState
 } from "./DotsResponseEvent";
 import { DotsRoom } from "./dotsRoom";
+import { DotsRequestEvent } from "./DotsRequestEvent";
 
 export enum DotsMatchResultState {
   inpro,
@@ -32,5 +34,109 @@ export class DotsMatch {
       }
       this.boardState.push(row);
     }
+  }
+
+  public processMove(event: DotsRequestEvent, cellNewState: number) {
+    // updateBoardState
+    const dotsEvent = event as DotsRequestEvent;
+    const { move } = event;
+    if (this.boardState[move.rowNum][move.colNum] !== 0) {
+      logger.error("Tampered data received");
+      return;
+    }
+    const isNewBoxFormed = this.updateAndCheckBoxFormed(
+      move.rowNum,
+      move.colNum,
+      cellNewState
+    );
+    // assign the nextTurnPlayerIndex based on whether
+    // this move resulted in formation of new box
+    if (!isNewBoxFormed) {
+      // if new box is not formed, then assign to other player
+      this.nextTurnPlayerIndex =
+        (this.nextTurnPlayerIndex + 1) % DotsRoom.maxPlayers;
+    }
+  }
+
+  public updateBoardScore() {
+    let player1Score = 0;
+    let player2Score = 0;
+    for (let r = 1; r < DotsMatch.BoardSize; r += 2) {
+      for (let c = 1; c < DotsMatch.BoardSize; c += 2) {
+        if (this.boardState[r][c] === 1) {
+          player1Score++;
+        } else if (this.boardState[r][c] === 2) {
+          player2Score++;
+        }
+      }
+    }
+    this.score = [player1Score, player2Score];
+  }
+
+  public updateMatchResult() {
+    if (this.score.reduce((a, b) => a + b, 0) === DotsMatch.MaxBoxNum) {
+      this.matchResult = DotsMatchResultState.result;
+    } else {
+      this.matchResult = DotsMatchResultState.inpro;
+    }
+  }
+
+  private updateAndCheckBoxFormed(
+    rowNum: number,
+    colNum: number,
+    cellNewState: number
+  ): boolean {
+    let boxFormed = false;
+    this.boardState[rowNum][colNum] = cellNewState;
+    if (rowNum % 2 !== 0) {
+      // horizontal line drawn
+      // if not the top most row horizontal line, then check if a box is formed just above
+      if (rowNum !== 0) {
+        const boxTopLine = this.boardState[rowNum - 2][colNum] !== 0;
+        const boxLtLine = this.boardState[rowNum - 1][colNum - 1] !== 0;
+        const boxRtLine = this.boardState[rowNum - 1][colNum + 1] !== 0;
+        const boxBtmLine = this.boardState[rowNum][colNum] !== 0; // redundant
+        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
+          this.boardState[rowNum - 1][colNum] = cellNewState;
+          boxFormed = true;
+        }
+      }
+      // if not the bottom most row horizontal line, then check if a box is formed just below
+      if (rowNum !== DotsMatch.BoardSize - 1) {
+        const boxTopLine = this.boardState[rowNum][colNum] !== 0; // redundant
+        const boxLtLine = this.boardState[rowNum + 1][colNum - 1] !== 0;
+        const boxRtLine = this.boardState[rowNum + 1][colNum + 1] !== 0;
+        const boxBtmLine = this.boardState[rowNum + 2][colNum] !== 0;
+        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
+          this.boardState[rowNum + 1][colNum] = cellNewState;
+          boxFormed = true;
+        }
+      }
+    } else {
+      // vertical line drawn
+      // if not the left most vertical line, then check if a box is formed just left
+      if (colNum !== 0) {
+        const boxTopLine = this.boardState[rowNum - 1][colNum - 1] !== 0;
+        const boxLtLine = this.boardState[rowNum][colNum - 2] !== 0;
+        const boxRtLine = this.boardState[rowNum][colNum] !== 0; // redundant
+        const boxBtmLine = this.boardState[rowNum + 1][colNum - 1] !== 0;
+        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
+          this.boardState[rowNum][colNum - 1] = cellNewState;
+          boxFormed = true;
+        }
+      }
+      // if not the right most vertical line, then check if a box is formed just right
+      if (colNum !== DotsMatch.BoardSize - 1) {
+        const boxTopLine = this.boardState[rowNum - 1][colNum + 1] !== 0;
+        const boxLtLine = this.boardState[rowNum][colNum] !== 0;
+        const boxRtLine = this.boardState[rowNum][colNum + 2] !== 0;
+        const boxBtmLine = this.boardState[rowNum + 1][colNum + 1] !== 0;
+        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
+          this.boardState[rowNum][colNum + 1] = cellNewState;
+          boxFormed = true;
+        }
+      }
+    }
+    return boxFormed;
   }
 }
--- src/dots/dotsRoom.ts
+++ src/dots/dotsRoom.ts
@@ -49,99 +49,11 @@ export class DotsRoom extends Room {
 
   private updateMatchState(event: any, player: Player) {
     const dotsEvent = event as DotsRequestEvent;
-    this.processMove(dotsEvent, player);
-    this.updateBoardScore();
-    this.updateMatchResult();
-    this.updateRoomScore();
-  }
-
-  private processMove(event: DotsRequestEvent, player: Player) {
-    // updateBoardState
     const cellNewState = this.getPlayerId(player) + 1;
-    const dotsEvent = <DotsRequestEvent>event;
-    const { move } = event;
-    if (this.match.boardState[move.rowNum][move.colNum] !== 0) {
-      logger.error("Tampered data received");
-      return;
-    }
-    const isNewBoxFormed = this.updateAndCheckBoxFormed(
-      move.rowNum,
-      move.colNum,
-      cellNewState
-    );
-    // assign the nextTurnPlayerIndex based on whether
-    // this move resulted in formation of new box
-    if (!isNewBoxFormed) {
-      // if new box is not formed, then assign to other player
-      this.match.nextTurnPlayerIndex =
-        (this.match.nextTurnPlayerIndex + 1) % DotsRoom.maxPlayers;
-    }
-  }
-
-  private updateAndCheckBoxFormed(
-    rowNum: number,
-    colNum: number,
-    cellNewState: number
-  ): boolean {
-    let boxFormed = false;
-    this.match.boardState[rowNum][colNum] = cellNewState;
-    if (rowNum % 2 !== 0) {
-      // horizontal line drawn
-      // if not the top most row horizontal line, then check if a box is formed just above
-      if (rowNum !== 0) {
-        const boxTopLine = this.match.boardState[rowNum - 2][colNum] !== 0;
-        const boxLtLine = this.match.boardState[rowNum - 1][colNum - 1] !== 0;
-        const boxRtLine = this.match.boardState[rowNum - 1][colNum + 1] !== 0;
-        const boxBtmLine = this.match.boardState[rowNum][colNum] !== 0; // redundant
-        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
-          this.match.boardState[rowNum - 1][colNum] = cellNewState;
-          boxFormed = true;
-        }
-      }
-      // if not the bottom most row horizontal line, then check if a box is formed just below
-      if (rowNum !== DotsMatch.BoardSize - 1) {
-        const boxTopLine = this.match.boardState[rowNum][colNum] !== 0; // redundant
-        const boxLtLine = this.match.boardState[rowNum + 1][colNum - 1] !== 0;
-        const boxRtLine = this.match.boardState[rowNum + 1][colNum + 1] !== 0;
-        const boxBtmLine = this.match.boardState[rowNum + 2][colNum] !== 0;
-        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
-          this.match.boardState[rowNum + 1][colNum] = cellNewState;
-          boxFormed = true;
-        }
-      }
-    } else {
-      // vertical line drawn
-      // if not the left most vertical line, then check if a box is formed just left
-      if (colNum !== 0) {
-        const boxTopLine = this.match.boardState[rowNum - 1][colNum - 1] !== 0;
-        const boxLtLine = this.match.boardState[rowNum][colNum - 2] !== 0;
-        const boxRtLine = this.match.boardState[rowNum][colNum] !== 0; // redundant
-        const boxBtmLine = this.match.boardState[rowNum + 1][colNum - 1] !== 0;
-        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
-          this.match.boardState[rowNum][colNum - 1] = cellNewState;
-          boxFormed = true;
-        }
-      }
-      // if not the right most vertical line, then check if a box is formed just right
-      if (colNum !== DotsMatch.BoardSize - 1) {
-        const boxTopLine = this.match.boardState[rowNum - 1][colNum + 1] !== 0;
-        const boxLtLine = this.match.boardState[rowNum][colNum] !== 0;
-        const boxRtLine = this.match.boardState[rowNum][colNum + 2] !== 0;
-        const boxBtmLine = this.match.boardState[rowNum + 1][colNum + 1] !== 0;
-        if (boxTopLine && boxLtLine && boxRtLine && boxBtmLine) {
-          this.match.boardState[rowNum][colNum + 1] = cellNewState;
-          boxFormed = true;
-        }
-      }
-    }
-    return boxFormed;
-  }
-  private updateMatchResult() {
-    if (this.match.score.reduce((a, b) => a + b, 0) === DotsMatch.MaxBoxNum) {
-      this.match.matchResult = DotsMatchResultState.result;
-    } else {
-      this.match.matchResult = DotsMatchResultState.inpro;
-    }
+    this.match.processMove(dotsEvent, cellNewState);
+    this.match.updateBoardScore();
+    this.match.updateMatchResult();
+    this.updateRoomScore();
   }
 
   private updateRoomScore() {
@@ -154,21 +66,6 @@ export class DotsRoom extends Room {
         this.score[2]++; // unnecessary as right now there are odd number of boxes available, so a winner is imminent
       }
     }
-  }
-
-  private updateBoardScore() {
-    let player1Score = 0;
-    let player2Score = 0;
-    for (let r = 1; r < DotsMatch.BoardSize; r += 2) {
-      for (let c = 1; c < DotsMatch.BoardSize; c += 2) {
-        if (this.match.boardState[r][c] === 1) {
-          player1Score++;
-        } else if (this.match.boardState[r][c] === 2) {
-          player2Score++;
-        }
-      }
-    }
-    this.match.score = [player1Score, player2Score];
   }
 
   private startGame() {

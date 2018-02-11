6b2c747226a3f247979e8032f2cf4b38fd2c0ac9
Abhishek Simla <abhishek.130490@gmail.com>
2018-02-11 19:03:43 +0530

    Cleaned up the score board

--- ui/src/components/scoreBoard/scoreBoard.js
+++ ui/src/components/scoreBoard/scoreBoard.js
@@ -7,9 +7,9 @@ const ScoreBoard = props => {
       <div className="score-cell header">Won</div>
       <div className="score-cell header">Ties</div>
       <div className="score-cell header">Lost</div>
-      <div className="score-cell">({props.won})</div>
-      <div className="score-cell">({props.ties})</div>
-      <div className="score-cell">({props.lost})</div>
+      <div className="score-cell">( {props.won} )</div>
+      <div className="score-cell">( {props.ties} )</div>
+      <div className="score-cell">( {props.lost} )</div>
     </div>
   );
 };

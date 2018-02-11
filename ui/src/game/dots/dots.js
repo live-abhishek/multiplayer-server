af36de0916ef7ac3fb8b1a6f769dda757c7d6a6d
Abhishek Simla <abhishek.130490@gmail.com>
2018-02-11 19:16:08 +0530

    Provided color to turns in dots

--- ui/src/game/dots/dots.js
+++ ui/src/game/dots/dots.js
@@ -30,9 +30,9 @@ class Dots extends React.Component {
     if (matchPos === "inpro" && turn === "wait") {
       return <LinearIndeterminate />;
     } else if (matchPos === "inpro" && turn === "me") {
-      return <div>Your turn </div>;
+      return <div className="dots-me dots-turn">Your turn </div>;
     } else if (matchPos === "inpro" && turn === "opp") {
-      return <div>Opp turn</div>;
+      return <div className="dots-opp dots-turn">Opp turn</div>;
     }
   };
 

0a6867727c54d0f9cc94141dc97211b0bec822b2
Abhishek Simla <abhishek.130490@gmail.com>
2018-02-11 20:17:08 +0530

    Tic tac toe cell content to 100%
    Formatted some code

--- ui/src/components/scoreBoard/scoreBoard.js
+++ ui/src/components/scoreBoard/scoreBoard.js
@@ -3,14 +3,23 @@ import "./scoreBoard.css";
 import smileySmiling from "../../images/happiness.png";
 import smileySad from "../../images/sad.png";
 import smileyConfused from "../../images/embarrassed.png";
+import Popout from "../../animations/popout";
 
 const ScoreBoard = props => {
   return (
     <div className="scoreBoard">
-      <div className="score-cell header"><img src={smileySmiling} alt="won"/></div>
-      {props.showTies && <div className="score-cell header"><img src={smileyConfused} alt="tied"/></div>}
+      <div className="score-cell header">
+        <img src={smileySmiling} alt="won" />
+      </div>
+      {props.showTies && (
+        <div className="score-cell header">
+          <img src={smileyConfused} alt="tied" />
+        </div>
+      )}
       {!props.showTies && <div />}
-      <div className="score-cell header"><img src={smileySad} alt="tied"/></div>
+      <div className="score-cell header">
+        <img src={smileySad} alt="tied" />
+      </div>
       <div className="score-cell">( {props.won} )</div>
       {props.showTies && <div className="score-cell">( {props.ties} )</div>}
       {!props.showTies && <div />}

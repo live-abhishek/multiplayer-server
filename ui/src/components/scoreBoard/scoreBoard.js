bc47eed396d849d64c170d82e9047fc7efd6dbad
Masterchief <live.abhishek@outlook.com>
2018-02-14 16:58:36 +0530

    Replaced scoreboard smilies with text

--- ui/src/components/scoreBoard/scoreBoard.js
+++ ui/src/components/scoreBoard/scoreBoard.js
@@ -8,18 +8,10 @@ import Popout from "../../animations/popout";
 const ScoreBoard = props => {
   return (
     <div className="scoreBoard">
-      <div className="score-cell header">
-        <img src={smileySmiling} alt="won" />
-      </div>
-      {props.showTies && (
-        <div className="score-cell header">
-          <img src={smileyConfused} alt="tied" />
-        </div>
-      )}
+      <div className="score-cell header">won</div>
+      {props.showTies && <div className="score-cell header">tied</div>}
       {!props.showTies && <div />}
-      <div className="score-cell header">
-        <img src={smileySad} alt="tied" />
-      </div>
+      <div className="score-cell header">lost</div>
       <div className="score-cell">( {props.won} )</div>
       {props.showTies && <div className="score-cell">( {props.ties} )</div>}
       {!props.showTies && <div />}
